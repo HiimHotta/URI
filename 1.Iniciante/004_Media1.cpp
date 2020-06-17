d2acbcb94cfe3b19265228dc46dd7a59fd8583da
HiimHotta <daniel.y.hotta@gmail.com>
2020-06-17 13:29:46 -0300

    Create 004_Media1.cpp

---
+++ 1.Iniciante/004_Media1.cpp
@@ -0,0 +1,38 @@
+/*******************************************************************************************************************
+Leia 2 valores de ponto flutuante de dupla precisão A e B, que correspondem a 2 notas de um aluno. A seguir, calcule
+a média do aluno, sabendo que a nota A tem peso 3.5 e a nota B tem peso 7.5 (A soma dos pesos portanto é 11). Assuma
+que cada nota pode ir de 0 até 10.0, sempre com uma casa decimal.
+
+Entrada
+O arquivo de entrada contém 2 valores com uma casa decimal cada um.
+
+Saída
+Imprima  a  mensagem  "MEDIA" e a média do aluno conforme exemplo abaixo, com 5 dígitos após o ponto decimal e com um
+espaço em branco antes e depois da igualdade. Utilize variáveis de dupla precisão (double) e como todos os problemas,
+não esqueça de imprimir o fim de linha após o resultado, caso contrário, você receberá "Presentation Error".
+
+*******************************************************************************************************************/
+
+#include <iostream>
+#include <iomanip>      // std::setprecision
+ 
+using namespace std;
+ 
+int main() {
+ 
+    /**
+     * Escreva a sua solução aqui
+     * Code your solution here
+     * Escriba su solución aquí
+     */
+     
+     double a, b, media;
+     
+     cin >> a; cin >> b;
+     
+     media = (a * 3.5 + b * 7.5) / 11;
+     
+     cout << fixed << setprecision (5) << "MEDIA = " << media << endl;
+ 
+    return 0;
+}
