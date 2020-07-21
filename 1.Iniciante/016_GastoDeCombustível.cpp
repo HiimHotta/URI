28c8565ab261942f6888ac2a577052b09b892c80
HiimHotta <daniel.y.hotta@gmail.com>
2020-07-21 16:44:46 -0300

    Create 016_GastoDeCombustível.cpp

---
+++ 1.Iniciante/016_GastoDeCombustível.cpp
@@ -0,0 +1,43 @@
+/*********************************************************************************************************
+Joaozinho  quer calcular e mostrar a quantidade de litros de combustível gastos em uma viagem, ao utilizar
+um automóvel que faz 12 KM/L. Para isso, ele gostaria que você o auxiliasse através de um simples programa.
+Para  efetuar  o cálculo, deve-se fornecer o tempo gasto na viagem (em horas) e a velocidade média durante
+a mesma (em km/h). Assim, pode-se obter distância percorrida e, em seguida, calcular quantos litros seriam
+necessários. Mostre o valor com 3 casas decimais após o ponto.
+
+
+Entrada
+
+
+O arquivo de entrada contém dois inteiros. O primeiro é o tempo gasto na viagem (em horas) e o segundo é
+a velocidade média durante a mesma (em km/h).
+
+
+Saída
+
+
+Imprima a quantidade de litros necessária para realizar a viagem, com três dígitos após o ponto decimal
+
+*********************************************************************************************************/
+
+#include <iostream>
+#include <iomanip>
+ 
+using namespace std;
+ 
+int main() {
+    
+    int t, vm; // tempo, velocidade
+    
+    double comb, efic = 12.0; // combustivel gasto e consumo combustivel
+    
+    cin >> t >> vm;
+    
+    comb = (vm * t) / efic;
+    
+    cout.precision (3);
+    
+    cout << fixed << comb << endl;
+ 
+    return 0;
+}
