4c5f9cb1613f71ca6c0e21ae7fb66e41ea561195
HiimHotta <daniel.y.hotta@gmail.com>
2021-03-25 17:43:21 -0300

    Create 036_Taxes.cpp

---
+++ 1.Iniciante/036_Taxes.cpp
@@ -0,0 +1,62 @@
+/******************************************************************************************************************
+In an imaginary country called Lisarb, all the people are very happy to pay their taxes because they know that
+doesn’t exist corrupt politicians and the taxes are used to benefit the population, without any misappropriation.
+ The currency of this country is Rombus, whose symbol is R$.
+
+Read a value with 2 digits after the decimal point, equivalent to the salary of a Lisarb inhabitant. Then print the
+ due value that this person must pay of taxes, according to the table below.
+
+
+
+Remember, if the salary is R$ 3,002.00 for example, the rate of 8% is only over R$ 1,000.00, because the salary
+from R$ 0.00 to R$ 2,000.00 is tax free. In the follow example, the total rate is 8% over R$ 1000.00 + 18% over
+R$ 2.00, resulting in R$ 80.36 at all. The answer must be printed with 2 digits after the decimal point.
+
+Input
+The input contains only a float-point number, with 2 digits after the decimal point.
+
+Output
+Print the message "R$" followed by a blank space and the total tax to be payed, with two digits after the decimal 
+point. If the value is up to 2000, print the message "Isento".
+******************************************************************************************************************/
+
+#include <iostream>
+#include <string.h>
+
+using namespace std;
+ 
+int main() {
+
+	cout.precision (2);
+
+    double salary, sum, salary_range[4] = {2000, 3000, 4500}, tax[4] = {0.00, 0.08, 0.18, 0.28};
+
+    cin >> salary;
+
+    if (salary < salary_range [0]) {
+    	cout << "Isento" << endl;
+    	return 0;
+    }
+
+    else if (salary < salary_range[1]) {
+    	sum = (salary - salary_range[0]) * tax[1];
+    }
+
+    else if (salary < salary_range[2]) {
+    	sum = (salary_range[1] -  salary_range[0]) * tax[1];
+
+    	sum += (salary - salary_range[1]) * tax[2]; 
+    }
+
+    else {
+    	sum = (salary_range[1] -  salary_range[0]) * tax[1];
+
+    	sum += (salary_range[2] - salary_range[1]) * tax[2];
+
+    	sum += (salary - salary_range[2]) * tax[3]; 
+    }
+
+    cout << "R$ " << fixed << sum << endl;
+
+    return 0;
+}
